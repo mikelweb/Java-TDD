71f1ba45edb72a24479566703dad49f1ac4de576
Mikel Goyeneche <info@mikelweb.net>
2025-05-24 20:12:14 +0200

    Update NumerosRomanosTest.java

--- src/test/java/org/example/numerosromanos/NumerosRomanosTest.java
+++ src/test/java/org/example/numerosromanos/NumerosRomanosTest.java
@@ -23,4 +23,21 @@ public class NumerosRomanosTest {
         String romano = numerosRomanos.convertirAromanos(3);
         assertEquals(romano, "III");
     }
+    @Test
+    public void seis() {
+        String romano = numerosRomanos.convertirAromanos(6);
+        assertEquals(romano, "VI");
+    }
+
+    @Test
+    public void siete() {
+        String romano = numerosRomanos.convertirAromanos(7);
+        assertEquals(romano, "VII");
+    }
+
+    @Test
+    public void ocho() {
+        String romano = numerosRomanos.convertirAromanos(8);
+        assertEquals(romano, "VIII");
+    }
 }
