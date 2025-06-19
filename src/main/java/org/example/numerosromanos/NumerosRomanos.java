3d920d4f9ae53ac0a0e6a484daa144abc3a481ac
Mikel Goyeneche <info@mikelweb.net>
2025-06-19 20:09:24 +0200

    Update NumerosRomanos.java

--- src/main/java/org/example/numerosromanos/NumerosRomanos.java
+++ src/main/java/org/example/numerosromanos/NumerosRomanos.java
@@ -12,6 +12,26 @@ public class NumerosRomanos {
                 return "IX";
             case 10:
                 return "X";
+            case 11:
+                return "XI";
+            case 12:
+                return "XII";
+            case 13:
+                return "XIII";
+            case 14:
+                return "XIV";
+            case 15:
+                return "XV";
+            case 16:
+                return "XVI";
+            case 17:
+                return "XVII";
+            case 18:
+                return "XVIII";
+            case 19:
+                return "XIX";
+            case 20:
+                return "XX";
         }
 
         if(numeroArabigo <= 3)
