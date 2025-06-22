767618acf355aa626e26309fb75d876100a62fa2
Mikel Goyeneche <info@mikelweb.net>
2025-06-22 23:31:00 +0200

    Update NumerosRomanos.java

--- src/main/java/org/example/numerosromanos/NumerosRomanos.java
+++ src/main/java/org/example/numerosromanos/NumerosRomanos.java
@@ -1,50 +1,84 @@
 package org.example.numerosromanos;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NumerosRomanos {
+
+    private final static List<String> simbolos = Arrays.asList("I","V","X","L","C","D","M");
+
     public String convertirAromanos(Integer numeroArabigo) {
 
-        switch (numeroArabigo) {
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            case 9:
-                return "IX";
-            case 10:
-                return "X";
-            case 11:
-                return "XI";
-            case 12:
-                return "XII";
-            case 13:
-                return "XIII";
-            case 14:
-                return "XIV";
-            case 15:
-                return "XV";
-            case 16:
-                return "XVI";
-            case 17:
-                return "XVII";
-            case 18:
-                return "XVIII";
-            case 19:
-                return "XIX";
-            case 20:
-                return "XX";
+        char[] numerosChar = numeroArabigo.toString().toCharArray();
+        
+        int incremento = 0;
+        String resultado = "";
+        for (int i = numerosChar.length - 1; i >= 0; i--) {
+            String romano = generico(Character.getNumericValue(numerosChar[i]), 0 + incremento, 1 + incremento, 2 + incremento);
+            resultado = romano + resultado;
+            incremento += 2;
         }
-
-        if(numeroArabigo <= 3)
-            return sumarI(1, numeroArabigo, "");
-
-        if(numeroArabigo <= 8)
-            return sumarI(6, numeroArabigo, "V");
-        return null;
+        return resultado;
     }
 
-    private String sumarI(int inicioCuenta, int num, String inicioNumRomano) {
+    private String generico(int decena, int x, int y, int z) {
+        switch (decena) {
+            case 4:
+                return simbolos.get(x) + simbolos.get(y);
+            case 9:
+                return simbolos.get(x) + simbolos.get(z);
+        }
+        if(decena <= 3)
+            return sumar(1, decena, "", simbolos.get(x));
+
+        if(decena <= 8)
+            return sumar(6, decena, simbolos.get(y), simbolos.get(x));
+
+        return null;
+
+    }
+
+/*
+    private String pasarDecena(int decena) {
+        switch (decena) {
+            case 4:
+                return "XL";
+            case 9:
+                return "XC";
+        }
+
+        if(decena <= 3)
+            return sumar(1, decena, "", "X");
+
+        if(decena <= 8)
+            return sumar(6, decena, "L", "X");
+
+        return null;
+
+    }
+
+
+    private String pasarUnidad(int unidad) {
+        switch (unidad) {
+            case 4:
+                return "IV";
+            case 9:
+                return "IX";
+        }
+
+        if(unidad <= 3)
+            return sumar(1, unidad, "", "I");
+
+        if(unidad <= 8)
+            return sumar(6, unidad, "V", "I");
+
+        return null;
+    }
+    */
+
+    private String sumar(int inicioCuenta, int num, String inicioNumRomano, String letra) {
         for(int i = inicioCuenta; i <= num; i++) {
-            inicioNumRomano += "I";
+            inicioNumRomano += letra;
         }
         return inicioNumRomano;
     }
